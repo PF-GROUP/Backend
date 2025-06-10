b55962a057c989f6b37dca435a2d30e5381bf773
Henry <soyhenryorozco@gmail.com>
2025-06-09 22:56:24 -0500

    initial commit

---
+++ dist/TypeOfProperty/typeofproperty.entity.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Typeofproperty = void 0;
+class Typeofproperty {
+}
+exports.Typeofproperty = Typeofproperty;
+//# sourceMappingURL=typeofproperty.entity.js.map
