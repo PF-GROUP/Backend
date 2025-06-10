ecf4982057a01050784f7644665a32f29ca751a0
Henry <soyhenryorozco@gmail.com>
2025-06-10 18:04:20 -0500

    feat: update Agency dto

--- src/Agency/create-agency.dto.ts
+++ src/Agency/create-agency.dto.ts
@@ -1,1 +1,14 @@
-export class CreateAgencyDto {}
+import { CustomizationDTO } from "src/Interface/Customization";
+import { PropertyDTO } from "src/Interface/Property";
+import { UserDTO } from "src/Interface/User";
+
+export class CreateAgencyDto {
+  id: number;
+  name: string;
+  description: string;
+  customization: CustomizationDTO;
+  properties: PropertyDTO[]; 
+  agentUser: UserDTO; 
+  cuit_dni_m: string; 
+
+}
