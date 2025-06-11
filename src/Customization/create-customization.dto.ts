7943afd1a1cd858d6309aaa75130afd30c32ab08
Jorge Nicolas Fernandez Gamarra <nicolas.fernandezgamarra@gmail.com>
2025-06-11 18:17:02 -0300

    entidad creada

--- src/Customization/create-customization.dto.ts
+++ src/Customization/create-customization.dto.ts
@@ -1,12 +1,36 @@
+import { Entity, PrimaryGeneratedColumn, Column, OneToOne } from "typeorm";
+import { Agency } from "src/Agency/agency.entity";
+
+@Entity('customizations')
 export class CreateCustomizationDto {
+  @PrimaryGeneratedColumn()
   id: number;
+
+  @Column({nullable: true})
   logoImage: string;
+  
+  @Column({nullable: true})
   information: string;
+
+  @Column({nullable: true})
   mainColors: string;
+  
+  @Column({nullable: true})
   banner: string;
+
+  @Column({nullable: true})
   navbarColor: string;
+
+  @Column({nullable: true})
   buttonColor: string;
+
+  @Column({nullable: true})
   backgroundColor: string;
+
+  @Column({nullable: true})
   secondaryColor: string;
 
+  //descomentar cuando este creado la relacion en agencia
+  // @OneToOne(() => Agency, agency => agency.customizations)
+  // agency: Agency;
 }
