75d7e3766094772e9ef21940472c3c0f48243c7a
Jorge Nicolas Fernandez Gamarra <nicolas.fernandezgamarra@gmail.com>
2025-06-11 20:17:32 -0300

    entidad de imagenes finalizado

--- src/Images/images.module.ts
+++ src/Images/images.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
 import { ImagesService } from './images.service';
 import { ImagesController } from './images.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Media } from './image.entity';
+import { Property } from 'src/Property/property.entity';
+
 
 @Module({
+  imports:[TypeOrmModule.forFeature([Media, Property])],
   controllers: [ImagesController],
   providers: [ImagesService],
+  exports: [TypeOrmModule, ImagesService]
 })
 export class ImagesModule {}
--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,8 @@ import { AppService } from './app.service';
 import { ConfigService, ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import typeorm from './Config/typeorm';
+import { ImagesModule } from './Images/images.module';
+import { CustomizationModule } from './Customization/customization.module';
 
 @Module({
   imports: [    ConfigModule.forRoot({
@@ -15,7 +17,9 @@ import typeorm from './Config/typeorm';
 
       // eslint-disable-next-line @typescript-eslint/no-unsafe-return
       useFactory: (config: ConfigService) => config.get('typeorm')!,
-    })],
+    }),
+    ImagesModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
