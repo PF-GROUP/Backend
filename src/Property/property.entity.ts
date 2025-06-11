60220f7eadcd1324f4a0e55c5ac55f93e2c63794
Akl-DGaletto <danielgenarog@gmail.com>
2025-06-11 17:06:02 -0300

    feat: implement Property entity with relationships and update module integration

--- src/Property/property.entity.ts
+++ src/Property/property.entity.ts
@@ -1,1 +1,68 @@
-export class Property {}
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  CreateDateColumn,
+  UpdateDateColumn,
+  OneToMany,
+  ManyToOne,
+  JoinColumn,
+} from 'typeorm';
+import { Typeofproperty } from '../TypeOfProperty/typeofproperty.entity';
+import { PropertyStatus } from './property-status.enum';
+import { Image } from 'src/Images/image.entity';
+import { PropertyType } from './property-type.enum';
+import { Agency } from 'src/Agency/agency.entity';
+
+@Entity('properties')
+export class Property {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  name: string;
+
+  @Column('enum', { enum: PropertyStatus })
+  status: PropertyStatus;
+
+  @Column('enum', { enum: PropertyType })
+  type: PropertyType;
+
+  @Column()
+  address: string;
+
+  @Column()
+  city: string;
+
+  @Column()
+  price: number;
+
+  @Column()
+  m2: number;
+
+  @Column()
+  bathrooms: number;
+
+  @Column()
+  description: string;
+
+  @Column()
+  rooms: number;
+
+  @CreateDateColumn({ name: 'created_at' })
+  createdAt: Date;
+
+  @UpdateDateColumn({ name: 'updated_at' })
+  updatedAt: Date;
+
+  @OneToMany(() => Image, (image) => image.property) // Relacion con la tabla de imagenes con array
+  id_images: Image[];
+
+  @ManyToOne(() => Typeofproperty, (type) => type.properties) // Relacion con la tabla de tipo de propiedad
+  @JoinColumn({ name: 'type_of_property_id' })
+  type_of_property: Typeofproperty;
+
+  @ManyToOne(() => Agency, (agency) => agency.properties) // Relacion con la tabla de agencia
+  @JoinColumn({ name: 'agency_id' })
+  agency: Agency;
+}
--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,7 @@ import { AppService } from './app.service';
 import { ConfigService, ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import typeorm from './Config/typeorm';
+import { PropertyModule } from './Property/property.module';
 
 @Module({
   imports: [    ConfigModule.forRoot({
@@ -15,7 +16,8 @@ import typeorm from './Config/typeorm';
 
       // eslint-disable-next-line @typescript-eslint/no-unsafe-return
       useFactory: (config: ConfigService) => config.get('typeorm')!,
-    })],
+    }),
+   PropertyModule],
   controllers: [AppController],
   providers: [AppService],
 })
