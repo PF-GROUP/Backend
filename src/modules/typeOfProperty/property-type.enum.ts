2c426fc5792e6155b104200a14502dc00578b7aa
Jorge Nicolas Fernandez Gamarra <nicolas.fernandezgamarra@gmail.com>
2025-06-17 18:44:40 -0300

    CRUD de typeofproperty terminada

---
+++ src/modules/typeOfProperty/property-type.enum.ts
@@ -0,0 +1,9 @@
+export enum PropertyTypeName {
+  CASA = 'Casa',
+  DEPARTAMENTO = 'Departamento',
+  CHALET = 'Chalet',
+  LOCAL_COMERCIAL = 'Local Comercial',
+  TERRENO = 'Terreno',
+  OFICINA = 'Oficina',
+  GALPON = 'Galpón',
+}
