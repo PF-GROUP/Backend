dad2d4a7434cd4306367f4e00ad7d5560b22ad65
PF-Proyect <kasapproyecto@gmail.com>
2025-07-01 18:26:07 +0000

    aaaa

--- src/modules/stripe/stripe.module.ts
+++ src/modules/stripe/stripe.module.ts
@@ -4,10 +4,11 @@ import { StripeController } from './stripe.controller';
 import { AgencyModule } from '../agency/agency.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Invoice, Suscription } from './stripe.collections.entity';
+import { UserModule } from '../user/user.module';
 
 @Module({
     imports:[
-        TypeOrmModule.forFeature([Suscription,Invoice]),
+        TypeOrmModule.forFeature([Suscription,Invoice]),UserModule,
         AgencyModule
     ],
     providers:[StripeService],
