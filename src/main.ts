c3523e40114c5716a47e80d7cf4f3b1c85e26ac9
fabianaledesma <fabianaaledesma@hotamail.com>
2026-01-21 14:17:43 -0300

    Simplify CORS configuration by allowing all origins and removing specific origin checks

--- src/main.ts
+++ src/main.ts
@@ -6,41 +6,11 @@ import { loggerGlobal } from './middlewares/logger-global/logger-global.middlewa
 import * as express from 'express';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.use((req, res, next) => {
-  if (req.method === 'OPTIONS') {
-    res.header('Access-Control-Allow-Origin', req.headers.origin);
-    res.header('Access-Control-Allow-Credentials', 'true');
-    res.header(
-      'Access-Control-Allow-Headers',
-      'Content-Type, Authorization'
-    );
-    res.header(
-      'Access-Control-Allow-Methods',
-      'GET,POST,PUT,PATCH,DELETE,OPTIONS'
-    );
-    return res.sendStatus(204);
-  }
-  next();
-});
   app.enableCors({
-  origin: (origin, callback) => {
-    const allowedOrigins = [
-      'http://localhost:3000',
-      'http://localhost:3001',
-      'https://kasapp-preview.vercel.app',
-      'https://kasapp-preview-jb4oz0f64-kasapp-preview.vercel.app',
-    ];
-
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, origin);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
-  methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
-  credentials: true,
-});
+    origin: ["*"],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
   app.use('/stripe/webhook', express.raw({ type: 'application/json' }));
 
   const swaggerConfig = new DocumentBuilder()
